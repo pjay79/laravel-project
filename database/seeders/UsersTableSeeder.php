bb1df72d9f695370c2d367961b5c5185ff7a8582
pjay79 <praveen.jayarajan@gmail.com>
2020-09-19 21:32:38 +1000

    create Article model, create Database seeders for Articles and Users

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        $password = Hash::make('toptal');
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@test.com',
+            'password' => $password,
+        ]);
+
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+        }
+    }
+}
